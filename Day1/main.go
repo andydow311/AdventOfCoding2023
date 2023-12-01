8196d86366b97c7b515f6ad1c7cf5601afcd8d99
Andy Dowell <andrewdowell_84@hotmail.com>
2023-12-01 10:38:41 +0000

    day 1 edit spaces

--- Day1/main.go
+++ Day1/main.go
@@ -10,26 +10,20 @@ import (
 )
 
 func lines(filename string) []string {
-
 	output := []string{}
-
 	readFile, err := os.Open(filename)
 
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	fileScanner := bufio.NewScanner(readFile)
-
 	fileScanner.Split(bufio.ScanLines)
-
 	for fileScanner.Scan() {
 		output = append(output, fileScanner.Text())
 	}
-
 	readFile.Close()
-
 	return output
-
 }
 
 func main() {
