94d9d251d8d1f14c0d71bcaae1b5be36aaaa714d
Andy Dowell <andrewdowell_84@hotmail.com>
2023-12-03 14:47:19 +0000

    day 3 refactor

--- Day3/main.go
+++ Day3/main.go
@@ -30,34 +30,11 @@ func lines(filename string) []string {
 }
 
 func main() {
-	lines := lines("input.txt")
-	fmt.Println("Part One: ", partOne(lines))
-	fmt.Println("Part Two: ", partTwo(lines))
-}
-
-func partOne(lines []string) int {
-	output := 0
-	for lineIndex, line := range lines {
-		theseLines := getLinesAboveAndBelow(lines, lineIndex, line)
-		digits := digitRegex.FindAllString(line, -1)
-		digitsIndex := digitRegex.FindAllStringIndex(line, -1)
-		for index := range digitsIndex {
-			startIndex := maximum(0, digitsIndex[index][0]-1)
-			endIndex := minimum(digitsIndex[index][1]+1, len(line)-1)
-			if isPart(theseLines, startIndex, endIndex) {
-				value, _ := strconv.Atoi(digits[index])
-				output = output + value
-			}
-		}
-
-	}
-	return output
-}
-
-func partTwo(lines []string) int {
-	output := 0
+	part:= os.Args[1]
+	partOne:= 0
+	partTwo:= 0
 	mapSymbolToPortNumber := make(map[string][]string)
-	
+	lines := lines("input.txt")
 	for lineIndex, line := range lines {
 		theseLines := getLinesAboveAndBelow(lines, lineIndex, line)
 		digits := digitRegex.FindAllString(line, -1)
@@ -65,32 +42,48 @@ func partTwo(lines []string) int {
 		for index := range digitsIndex {
 			startIndex := maximum(0, digitsIndex[index][0]-1)
 			endIndex := minimum(digitsIndex[index][1]+1, len(line)-1)
-			digit := digits[index]
-			if isPart(theseLines, startIndex, endIndex){
-				keyOk,line := partAdjacentToAsterix(theseLines, startIndex, endIndex)
-				if keyOk {
-					_, ok := mapSymbolToPortNumber[line]
-					if ok {
-						array := mapSymbolToPortNumber[line]
-						array = append(array, digit)
-						mapSymbolToPortNumber[line] = array
-					} else {
-						array := []string{digit}
-						mapSymbolToPortNumber[line] = array
-					}
-				}					
+			if part == "1"{
+				if isPart(theseLines, startIndex, endIndex) {
+					value, _ := strconv.Atoi(digits[index])
+					partOne = partOne + value
+				}
+			}else if part == "2"{
+				digit := digits[index]
+				if isPart(theseLines, startIndex, endIndex){
+					keyOk,line := isPartAdjacentToAsterix(theseLines, startIndex, endIndex)
+					if keyOk {
+						_, ok := mapSymbolToPortNumber[line]
+						if ok {
+							array := mapSymbolToPortNumber[line]
+							array = append(array, digit)
+							mapSymbolToPortNumber[line] = array
+						} else {
+							array := []string{digit}
+							mapSymbolToPortNumber[line] = array
+						}
+					}					
+				}
 			}
 		}
+
 	}
 
-	for key, value := range mapSymbolToPortNumber {
-		if len(value) == 2 {
-			firstRatio, _ := strconv.Atoi(mapSymbolToPortNumber[key][0])
-			secondRatio, _ := strconv.Atoi(mapSymbolToPortNumber[key][1])
-			output=output+(firstRatio*secondRatio)
+	if part == "1"{
+		fmt.Println("Part One: ", partOne)
+	}else if  part == "2"{
+		for key, value := range mapSymbolToPortNumber {
+			if len(value) == 2 {
+				firstRatio, _ := strconv.Atoi(mapSymbolToPortNumber[key][0])
+				secondRatio, _ := strconv.Atoi(mapSymbolToPortNumber[key][1])
+				partTwo=partTwo+(firstRatio*secondRatio)
+			}
 		}
+		fmt.Println("Part Two: ", partTwo)
+	}else{
+		fmt.Println("OS input not recognised")
 	}
-	return output
+	
+	
 }
 
 func maximum(a int, b int) int {
@@ -120,7 +113,7 @@ func isPart(theseLines []string, startIndex int, endIndex int) bool {
 	return false
 }
 
-func partAdjacentToAsterix(theseLines []string, startIndex int, endIndex int) (bool, string) {
+func isPartAdjacentToAsterix(theseLines []string, startIndex int, endIndex int) (bool, string) {
 	for _, line := range theseLines {
 		for i := startIndex; i < endIndex; i++ {
 			if gearRegex.MatchString(string(line[i])) {
